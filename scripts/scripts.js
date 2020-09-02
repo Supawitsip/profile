446389fd4a187f91621a8fc362a7383ed0dabf14
Supawit Sippasuchon <supawitsip@kkumail.com>
2020-09-02 16:04:29 +0700

    'FixUI'

---
+++ scripts/scripts.js
@@ -0,0 +1,23 @@
+
+function foolButton() {
+  document.getElementById("follow-button").innerHTML = "You fool!!!!";
+}
+/*
+function changeBackground() {
+  document.getElementById("demo").style.color = "red";
+}
+*/
+function changeColor(elmnt,clr) {
+  elmnt.style.color = clr;
+}
+/*
+function worldTime(id){
+  getElementById(id).innerHTML=Date();
+}*/
+ 
+/* buttonpromp.addEventListener('click', button)*/
+
+function button() {
+  var buttonpromp = document.getElementById('redcolor');
+  buttonpromp.style.color = red;
+}
